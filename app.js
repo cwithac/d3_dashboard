2b6f2fdd4a744d7d3c787751e410047282570c68
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-31 13:39:37 -0500

    Tool tip

--- app.js
+++ app.js
@@ -55,4 +55,52 @@ d3.queue()
               drawMap(geoData, data, currentYear, currentDataType);
               drawBar(data, currentDataType, country);
             });
-    });
+
+        d3.selectAll('svg')
+            .on('mousemove touchmove', updateTooltip);
+
+        function updateTooltip() {
+          const tooltip = d3.select('.tooltip');
+          const tgt = d3.select(d3.event.target);
+          const isCountry = tgt.classed('country');
+          const isBar = tgt.classed('bar');
+          const isArc = tgt.classed('arc');
+          const dataType = d3.select('input:checked')
+                              .property('value');
+          const units = dataType === 'emissions' ? 'thousand metrics tons' : 'metric tons per capita';
+          let data;
+          let percentage = '';
+          if (isCountry) data = tgt.data()[0].properties;
+          if (isArc) {
+            data = tgt.data()[0].data;
+            percentage = `<p>Percentage of total: ${getPercentage(tgt.data()[0])}</p>`;
+          }
+          if (isBar) data = tgt.data()[0];
+          tooltip
+              .style("opacity", +(isCountry || isArc || isBar))
+              .style("left", (d3.event.pageX - tooltip.node().offsetWidth / 2) + "px")
+              .style("top", (d3.event.pageY - tooltip.node().offsetHeight - 10) + "px");
+          if (data) {
+            var dataValue = data[dataType] ?
+              data[dataType].toLocaleString() + " " + units :
+              "Data Not Available";
+            tooltip
+                .html(`
+                  <p>Country: ${data.country}</p>
+                  <p>${formatDataType(dataType)}: ${dataValue}</p>
+                  <p>Year: ${data.year || d3.select("#year").property("value")}</p>
+                  ${percentage}
+                `)
+          }
+        }
+});
+
+function formatDataType(key) {
+  return key[0].toUpperCase() + key.slice(1).replace(/[A-Z]/g, c => " " + c);
+}
+
+function getPercentage(d) {
+  var angle = d.endAngle - d.startAngle;
+  var fraction = 100 * angle / (Math.PI * 2);
+  return fraction.toFixed(2) + "%";
+}
